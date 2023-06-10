422f961fc4240a9105f304dfd3e50b27fe44d16b
StammerOfficial <tee@stammerofficial.com>
2023-06-10 22:44:21 +0300

    Add wannabe virus

---
+++ Computer_Code.vb
@@ -0,0 +1,37 @@
+Harmless Virus
+--------------------
+X=MsgBox("Error while opening Computer code",1+64,"Computer") 
+
+X=MsgBox("Try again?",3+64,"Computer") 
+
+X=MsgBox("Scan code?",4+64,"Computer") 
+
+X=MsgBox("Error",5+64,"Computer") 
+
+X=MsgBox("Dangerous code!",0+48,"Computer") 
+
+X=MsgBox("Delete code?",3+64,"Computer") 
+
+X=MsgBox("Error",2+64,"Computer") 
+
+X=MsgBox("...",0+16,"...") 
+
+X=MsgBox("Dele-...-Hacked!",0+16,"Com-..-t-..-...") 
+
+X=MsgBox(".",0+16,"Title") 
+
+X=MsgBox("..",0+16,"Title") 
+
+X=MsgBox("...",0+16,"Title") 
+
+X=MsgBox("..",0+16,"Title") 
+
+X=MsgBox(".",0+16,"Title") 
+
+X=MsgBox("Scanning Password",0+16,"Co-..") 
+
+X=MsgBox("Shutting down computer!",0+16,"Title") 
+
+X=MsgBox("LOL Prank!",1+64,"Computer code") 
+
+X=MsgBox("Bye?",0+64,"Computer code") 
